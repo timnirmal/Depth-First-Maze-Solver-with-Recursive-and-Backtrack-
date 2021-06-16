592c9a599f8845906828a68c05873a43ffdb4f29
thimira nirmal <timnirmal@gmail.com>
2021-06-16 17:53:06 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ 0 - Path
+ 1 - Wall
+ A - Starting Point
+ B - Finish Point
+
+ The generated puzzle must be saved to a text file
+
+ //For Solution
+ Path should replace with // and saved to another text file.
+*/
+#include<iostream>
+#include<bits/stdc++.h>
+#include <algorithm>
+#include "windows.h"
+
+using namespace std;
+
+int maze_size = 21;
+int maze
